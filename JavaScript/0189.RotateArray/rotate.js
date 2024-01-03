c91afb3ac47ae0bb4e5fac202706fac4d580e811
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-01-03 15:37:59 -0700

    189. Rotate Array

--- JavaScript/0189.RotateArray/rotate.js
+++ JavaScript/0189.RotateArray/rotate.js
@@ -7,8 +7,4 @@ var rotate = function (nums, k) {
     k = k % nums.length;
     let part = nums.splice(nums.length - k);
     nums.unshift(...part);
-    
-    return nums;
 };
-
-console.log(rotate([1,2,3,4,5,6,7], 3))
